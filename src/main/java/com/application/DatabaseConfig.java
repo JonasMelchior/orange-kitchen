b5bd734ffdcd2f2c3b5b82a87ba49eb9579c261c
Gurkefar <jonasjensen04@gmail.com>
2022-08-23 14:11:14 +0200

    Changed primary color to orange

--- src/main/java/com/application/DatabaseConfig.java
+++ src/main/java/com/application/DatabaseConfig.java
@@ -4,9 +4,11 @@ import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
+
 *//*
-* For deployment at heroku
-* *//*
+For deployment at heroku
+*//*
+
 @Configuration
 public class DatabaseConfig {
 

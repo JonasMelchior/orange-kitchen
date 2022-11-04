056647e2b8a0552dc345754dfa9b695f04292429
Gurkefar <jonasjensen04@gmail.com>
2022-11-04 12:16:13 +0100

    changed configurations database

--- src/main/java/com/application/DatabaseConfig.java
+++ src/main/java/com/application/DatabaseConfig.java
@@ -1,14 +1,17 @@
+/*
 package com.application;
 
 import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
+*/
 /*
 
 For deployment at heroku
 
-*/
+*//*
+
 @Configuration
 public class DatabaseConfig {
 
@@ -22,3 +25,4 @@ public class DatabaseConfig {
         return new HikariDataSource(config);
     }
 }
+*/
